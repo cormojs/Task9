1fb329c78e39df6683ada46479cd86d4a13c0c71
cormojs <cormorant2+github@gmail.com>
2014-07-15 21:13:33 +0900

    Add Thumbnails.hs

--- Main.hs
+++ Main.hs
@@ -2,27 +2,20 @@
 module Main where
 
 
--- Private
+-- Internal
 import Types
+import Thumbnails
 
 -- Gtk
 import Graphics.UI.Gtk (AttrOp((:=)), set, on)
 
 import qualified Graphics.UI.Gtk.Abstract.Container as AContainer
 import qualified Graphics.UI.Gtk.Abstract.Widget    as AWidget
-import qualified Graphics.UI.Gtk.General.Enums      as GEnums
 import qualified Graphics.UI.Gtk.General.General    as GGeneral
 import qualified Graphics.UI.Gtk.Windows.Window     as WWindow
-import qualified Graphics.UI.Gtk.ModelView          as MV
-import qualified Graphics.UI.Gtk.Scrolling.ScrolledWindow as SScrolled
-import qualified Graphics.UI.Gtk.Gdk.Pixbuf         as GPixbuf
 import qualified Graphics.UI.Gtk.Layout.Notebook    as LNotebook
 
 -- File operation
-import qualified System.Directory as Dir
-import qualified System.FilePath  as FilePath
-
-import System.FilePath     ((</>))
 import System.Environment  (getArgs)
 
 -- Control
@@ -34,7 +27,7 @@ import qualified Control.Monad.Trans as Trans
 main :: IO ()
 main = do
   GGeneral.initGUI
-  
+
   Thumbnails { thumbsView  = thView
              , thumbsModel = thModel } <- thumbnailsNew
   Monad.mapM_ (populateStoreFromDir thModel) =<< getArgs
@@ -50,45 +43,3 @@ main = do
   AWidget.widgetShowAll window
   
   GGeneral.mainGUI
-
-populateStoreFromDir store dir = do
-  let isPict f = FilePath.takeExtension f `elem` [".jpg", ".jpeg", ".png"]
-  files <-
-    Monad.filterM Dir.doesFileExist
-    =<< (map (dir</>) <$> Dir.getDirectoryContents dir)
-  Monad.forM_ (filter isPict files) $ \filename -> do
-    pixbuf <- GPixbuf.pixbufNewFromFileAtSize filename 240 300
-    store `MV.listStoreAppend` (filename, pixbuf)
-
-thumbnailsNew :: IO Thumbnails
-thumbnailsNew = do
-  imgStore <- setupStore
-
-  imgStoreSorted <- MV.treeModelSortNewWithModel imgStore
-  MV.treeSortableSetSortFunc imgStoreSorted 1 (compareRow imgStore fst)
-  MV.treeSortableSetSortColumnId imgStoreSorted 1 GEnums.SortDescending
-
-  imgView <- MV.iconViewNewWithModel imgStoreSorted
-  imgView `set` [ MV.iconViewModel        := Just imgStoreSorted
-                , MV.iconViewPixbufColumn := MV.makeColumnIdPixbuf 1
-                , MV.iconViewTextColumn   := MV.makeColumnIdString 0 ]
-
-  scrolled <- SScrolled.scrolledWindowNew Nothing Nothing
-  scrolled `set` [ AContainer.containerChild := imgView ]
-
-  return Thumbnails { thumbsModel = imgStore, thumbsView = scrolled }
-  where
-    -- store(model)を作成
-    setupStore :: IO (MV.ListStore MyImage)
-    setupStore = do
-      store <- MV.listStoreNew ([] :: [MyImage])
-      MV.customStoreSetColumn store (MV.makeColumnIdString 0) (FilePath.takeFileName . fst)
-      MV.customStoreSetColumn store (MV.makeColumnIdPixbuf 1) snd
-      return store
-    compareRow :: Ord b => MV.ListStore a -> (a -> b)
-                  -> MV.TreeIter -> MV.TreeIter -> IO Ordering
-    compareRow store acc iter1 iter2 = do
-      v1 <- MV.treeModelGetRow store iter1
-      v2 <- MV.treeModelGetRow store iter2
-      return $ acc v1 `compare` acc v2
-
---
+++ Thumbnails.hs
@@ -0,0 +1,64 @@
+module Thumbnails where
+
+-- Internal
+import Types
+
+-- Gtk
+import Graphics.UI.Gtk (AttrOp((:=)), set, on)
+
+import qualified Graphics.UI.Gtk.Abstract.Container as AContainer
+import qualified Graphics.UI.Gtk.General.Enums      as GEnums
+import qualified Graphics.UI.Gtk.ModelView          as MV
+import qualified Graphics.UI.Gtk.Scrolling.ScrolledWindow as SScrolled
+import qualified Graphics.UI.Gtk.Gdk.Pixbuf         as GPixbuf
+
+-- System
+import qualified System.Directory as Dir
+import qualified System.FilePath  as FilePath
+import System.FilePath     ((</>))
+
+-- Control
+import qualified Control.Monad       as Monad
+import Control.Applicative ((<$>))
+
+
+thumbnailsNew :: IO Thumbnails
+thumbnailsNew = do
+  imgStore <- thumbnailsStoreNew
+
+  imgStoreSorted <- MV.treeModelSortNewWithModel imgStore
+  MV.treeSortableSetSortFunc imgStoreSorted 1 (compareRow imgStore fst)
+  MV.treeSortableSetSortColumnId imgStoreSorted 1 GEnums.SortDescending
+
+  imgView <- MV.iconViewNewWithModel imgStoreSorted
+  imgView `set` [ MV.iconViewModel        := Just imgStoreSorted
+                , MV.iconViewPixbufColumn := MV.makeColumnIdPixbuf 1
+                , MV.iconViewTextColumn   := MV.makeColumnIdString 0 ]
+
+  scrolled <- SScrolled.scrolledWindowNew Nothing Nothing
+  scrolled `set` [ AContainer.containerChild := imgView ]
+
+  return Thumbnails { thumbsModel = imgStore, thumbsView = scrolled }
+  where
+    compareRow :: Ord b => MV.ListStore a -> (a -> b)
+                  -> MV.TreeIter -> MV.TreeIter -> IO Ordering
+    compareRow store acc iter1 iter2 = do
+      v1 <- MV.treeModelGetRow store iter1
+      v2 <- MV.treeModelGetRow store iter2
+      return $ acc v1 `compare` acc v2
+
+thumbnailsStoreNew :: IO (MV.ListStore MyImage)
+thumbnailsStoreNew = do
+  store <- MV.listStoreNew ([] :: [MyImage])
+  MV.customStoreSetColumn store (MV.makeColumnIdString 0) (FilePath.takeFileName . fst)
+  MV.customStoreSetColumn store (MV.makeColumnIdPixbuf 1) snd
+  return store
+
+populateStoreFromDir :: MV.ListStore MyImage -> String -> IO ()
+populateStoreFromDir store dir = do
+  let isPict f = FilePath.takeExtension f `elem` [".jpg", ".jpeg", ".png"]
+  files <- Monad.filterM Dir.doesFileExist
+           =<< (map (dir</>) <$> Dir.getDirectoryContents dir)
+  Monad.forM_ (filter isPict files) $ \filename -> do
+    pixbuf <- GPixbuf.pixbufNewFromFileAtSize filename 240 300
+    store `MV.listStoreAppend` (filename, pixbuf)
